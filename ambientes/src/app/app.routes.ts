7c8b46e1de278f0ab0b8319378af65937c5cc1b0
maicol <maicol10borja@gmail.com>
2025-07-08 09:00:02 -0500

    Nuevo

--- ambientes/src/app/app.routes.ts
+++ ambientes/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { FooterComponent } from './components/footer/footer';
+import { HomeComponent } from './components/home/home';
+import { NavbarComponent } from './components/navbar/navbar';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'footer', component: FooterComponent },
+  { path: 'navbar', component: NavbarComponent }
+];
